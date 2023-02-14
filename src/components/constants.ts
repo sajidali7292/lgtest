25ad2ad28f4126405aa6a746e604e89fb78c753f
femiss <felipe@linkgraph.io>
2023-02-14 14:00:13 -0600

    constants for component

---
+++ src/components/constants.ts
@@ -0,0 +1,21 @@
+export const backgroundColors = {
+    yellow: "bg-yellow text-black",
+    dark: "bg-semi_dark text-white"
+}
+
+export const sectionVariantsTop = {
+    xs: 'pt-7 lg:pt-14',
+    sm: 'pt-8 lg:pt-16',
+    md: 'pt-8 lg:pt-24',
+    lg: 'pt-9 lg:pt-28',
+    xl: 'pt-10 lg:pt-32',
+    xxl: 'pt-14 lg:pt-40',
+}
+export const sectionVariantsBottom = {
+    xs: 'pb-7 lg:pb-14',
+    sm: 'pb-8 lg:pb-16',
+    md: 'pb-8 lg:pb-24',
+    lg: 'pb-9 lg:pb-28',
+    xl: 'pb-10 lg:pb-32',
+    xxl: 'pb-14 lg:pb-40',
+}
