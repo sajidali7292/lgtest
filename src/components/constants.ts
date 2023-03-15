548716a2ddce2240641950be93360a086abd0f50
femiss <felipe@linkgraph.io>
2023-03-15 13:12:35 -0600

    updated constants

--- src/components/constants.ts
+++ src/components/constants.ts
@@ -8,17 +8,17 @@ export const sectionVariantsTop = {
     zr: 'pt-0',
     xs: 'pt-7 lg:pt-14',
     sm: 'pt-8 lg:pt-16',
-    md: 'pt-8 lg:pt-24',
-    lg: 'pt-9 lg:pt-28',
-    xl: 'pt-10 lg:pt-32',
+    md: 'pt-9 lg:pt-24',
+    lg: 'pt-10 lg:pt-28',
+    xl: 'pt-11 lg:pt-32',
     xxl: 'pt-14 lg:pt-40',
 }
 export const sectionVariantsBottom = {
     zr: 'pb-0',
     xs: 'pb-7 lg:pb-14',
     sm: 'pb-8 lg:pb-16',
-    md: 'pb-8 lg:pb-24',
-    lg: 'pb-9 lg:pb-28',
-    xl: 'pb-10 lg:pb-32',
+    md: 'pb-9 lg:pb-24',
+    lg: 'pb-10 lg:pb-28',
+    xl: 'pb-11 lg:pb-32',
     xxl: 'pb-14 lg:pb-40',
 }
