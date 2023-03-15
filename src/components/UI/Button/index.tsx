5ac70f09621f61080fa2d7e4012856471ad6533c
femiss <felipe@linkgraph.io>
2023-03-15 13:15:50 -0600

    updated button

--- src/components/UI/Button/index.tsx
+++ src/components/UI/Button/index.tsx
@@ -3,11 +3,11 @@ import { ReactSVG } from "react-svg";
 
 //RENDER LINK A
 const button = ({ buttonObj }) => {
-    const btnType = buttonObj.type == 'secondary' ? 'button-secondary': buttonObj.type === 'third' ? 'button-third':'';
+    // const btnType = buttonObj.type == 'secondary' ? 'button-secondary': buttonObj.type === 'third' ? 'button-third':'';
     return (
         <Link href={buttonObj.url}>
             <a href={buttonObj.url}
-            className={`button ${btnType}
+            className={`button button-${buttonObj.type}
             ${buttonObj.rounded ? 'rounded':''}
             ${buttonObj.size == 'medium' ? 'medium': buttonObj.size = 'big' ? 'big':''}
             ${buttonObj.icon ? 'icon':''}
