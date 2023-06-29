6778fdfc155e975f606ed26218f579064f17c1ef
femiss <felipe@linkgraph.io>
2023-06-29 10:57:59 -0600

    New | HubSpot Common

--- src/components/HomeFormSection.tsx
+++ src/components/HomeFormSection.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import styles from 'scss/components/HubspotForm.module.scss';
-import HubspotForm from 'react-hubspot-form';
+import Image from 'next/image'
+import dynamic from 'next/dynamic'
+
+const HubspotForm = dynamic(
+  () => import('react-hubspot-form'),
+  { ssr: false }
+)
 
 interface ClientLogo {
   client_logo?: {
@@ -10,6 +16,7 @@ interface ClientLogo {
 }
 
 interface Props {
+  className?: string;
   title?: string;
   text?: string;
   photo?: {
@@ -30,6 +37,7 @@ interface Props {
 }
 
 function HomeFormSection({
+  className,
   title,
   text,
   photo,
@@ -43,45 +51,68 @@ function HomeFormSection({
   };
 
   return (
-    <section className={`${styles.LaHome_form}`} style={{ backgroundColor: '#3E255A' }}>
-      <div className={styles.container}>
+    <section className={`${styles.LaHome_form} ${className ? styles[className] : ''}`} style={{ backgroundColor: '#3E255A' }}>
+      <div className={`${styles.container} container`}>
         <div className="flex flex-row">
-          <div className="md:basis-full basis-6/12">
+          <div className="basis-6/12">
             <h2 className={`${styles.title}`}>{title}</h2>
-            <img src={process.env.NEXT_PUBLIC_WORDPRESS_URL + '/wp-content/uploads/2023/03/svgexport-13.svg'} className={`${styles.quote}`} alt='quote mark' />
+            <Image
+                src={process.env.NEXT_PUBLIC_WORDPRESS_URL + '/wp-content/uploads/2023/03/svgexport-13.svg'}
+                alt="Quote Icon"
+                className={`${styles.quote}`}
+                width={29}
+                height={22}
+            />
             <p className={`${styles.txt}`}>{text}</p>
-            <div className="flex flex-row">
-              <div className="basis-3/12">
-                <img src={photo.url} alt={photo.alt} />
+            <div className={`flex flex-row justify-center ${styles.client_wrap}`}>
+              <div className="flex-auto"></div>
+              <div className="flex-none">
+                <Image
+                    src={photo.url}
+                    alt={photo.alt}
+                    className={`${styles.quote}`}
+                    width={62}
+                    height={62}
+                />
               </div>
-              <div className="basis-7/12">
+              <div className={`flex-3 ${styles.cliet_txt_wrapper}`}>
                 <div className={`${styles.mpClientName}`}>{client_name}</div>
                 <div className={`${styles.mpClientPosition}`}>{client_position}</div>
               </div>
             </div>
-            <div className={`${styles.form_logos} flex flex-row`}>
+            <div className={`${styles.form_logos} justify-center flex flex-row`}>
               {form_logos.map((clientlogo, index) => (
-                <div className="md:basis-auto basis-3/12" key={index}>
+                <div className="basis-auto lg:basis-3/12" key={index}>
                   <img src={clientlogo.client_logo.url} alt={clientlogo.client_logo.alt} />
                 </div>
               ))}
             </div>
           </div>
-          <div className="md:basis-full basis-1/12"></div>
-          <div className="md:basis-full basis-5/12">
-            <div className={`${styles.mpformWp}`}>
-              <div className={`${styles.mpGetStarted}`}><img src={process.env.NEXT_PUBLIC_WORDPRESS_URL + '/wp-content/uploads/2023/03/svgexport-14.svg'} alt="Get Started" className={`${styles.mpGetS}`} /></div>
-              <div className={`${styles.mp__formSec}`}>
-                {hubspotFormProps && (
-                  <HubspotForm
-                    portalId={hubspotFormProps.portalId}
-                    formId={hubspotFormProps.formId}
-                    css={hubspotFormProps.css}
-                    onSubmit={hubspotFormProps.onSubmit}
-                    loading={hubspotFormProps.loading}
-                    onReady={handleFormReady}
+          <div className="basis-1/12"></div>
+          <div className={`basis-5/12`}>
+            <div className={`${styles.wrapper}`}>
+              <div className={`${styles.form_container}`}>
+                <div className={`${styles.get_starte_img}`}>
+                  <Image
+                      src={process.env.NEXT_PUBLIC_WORDPRESS_URL + '/wp-content/uploads/2023/03/svgexport-14.svg'}
+                      alt="Get Started"
+                      className={`${styles.mpGetS}`}
+                      width={90}
+                      height={66}
                   />
-                )}
+                </div>
+                <div className={`common_form_wrapper`}>
+                  {hubspotFormProps && (
+                    <HubspotForm
+                      portalId={hubspotFormProps.portalId}
+                      formId={hubspotFormProps.formId}
+                      css={hubspotFormProps.css}
+                      onSubmit={hubspotFormProps.onSubmit}
+                      loading={hubspotFormProps.loading}
+                      onReady={handleFormReady}
+                    />
+                  )}
+                </div>
               </div>
             </div>
           </div>
