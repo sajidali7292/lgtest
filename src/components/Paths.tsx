ea1b05c8f85100265d1c459909a9807d3a498b1f
femiss <felipe@linkgraph.io>
2023-06-29 10:58:54 -0600

    New | Paths Dynamic

--- src/components/Paths.tsx
+++ src/components/Paths.tsx
@@ -1,43 +1,43 @@
 import React from 'react';
 import styles from 'scss/components/Paths.module.scss';
 import Button from './UI/Button/index';
-
-
-interface PathProps {
-  tag?: string;
-  title?: string;
-  txt?: string;
-  button?: any;
-  img?: {
-    url?: string;
-  };
-}
+import { sectionVariantsTop, sectionVariantsBottom } from './constants';
 
 interface Props {
   title?: string;
-  paths?: PathProps[];
+  paths?: Array<any>;
+  pt?: string;
+  pb?: string;
 }
 
-function Paths({ title, paths }: Props): JSX.Element {
+function Paths({ 
+  title,
+  paths,
+  pt = 'md',
+  pb = 'md', }: Props): JSX.Element {
+  const ptVariant = sectionVariantsTop[pt];
+  const pBVariant = sectionVariantsBottom[pb];
   return (
     <section className={styles.paths}>
-      <div className="container">
+      <div className={`container ${ptVariant} ${pBVariant}`}>
         <div className="row">
-          <div className="col-12">
+          <div className="basis-full">
             <h2 className={styles.paths_title}>{title}</h2>
           </div>
         </div>
-        <div className="flex flex-row">
+        <div className={`flex flex-row ${styles.img_wrapper}`}>
           {paths.map((path, index) => (
-            <div className="sm:basis-full md:basis-6/12 basis-1/12" key={`path-${index}`}>
+            <div className="flex-1" key={`path-${index}`}>
               <div
-                className={`${styles.pathimg_1} bg_img`}
+                className={`${styles.pathimg} bg_img`}
                 style={{ backgroundImage: `url(${path.img.url})` }}
               >
-                <div className={styles.path_sub1}>{path.tag}</div>
+                <div className={`${styles.path_sub1} tag tag_${path.tag.color}`}>{path.tag.name}</div>
                 <div className={styles.path_ttl}>{path.title}</div>
                 <div className={styles.path_txt}>{path.txt}</div>
-                {path.button && <Button buttonObj={path.button} />}
+                <div className={styles.path_btn}>
+                  {path.button && <Button buttonObj={path.button} />}
+                </div>
               </div>
             </div>
           ))}
