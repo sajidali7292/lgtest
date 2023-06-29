31d127af305dca027d63a8fd1d0460f3d5ed954f
femiss <felipe@linkgraph.io>
2023-06-29 11:04:58 -0600

    Added | New options on Button

--- src/components/UI/Button/index.tsx
+++ src/components/UI/Button/index.tsx
@@ -3,15 +3,16 @@ import { ReactSVG } from "react-svg";
 
 //RENDER LINK A
 const button = ({ buttonObj }) => {
-    // const btnType = buttonObj.type == 'secondary' ? 'button-secondary': buttonObj.type === 'third' ? 'button-third':'';
     return (
         <Link href={buttonObj.url}>
             <a href={buttonObj.url}
-            className={`button button-${buttonObj.type}
-            ${buttonObj.rounded ? 'rounded':''}
-            ${buttonObj.size == 'medium' ? 'medium': buttonObj.size = 'big' ? 'big':''}
+            className={`button button-${buttonObj.type} ${buttonObj.isLink ? 'button-link':''} ${buttonObj.isUnderline ? 'underline hover:underline':''}
+            ${buttonObj.hoverTransparent ? 'transparent_effect':''}
+            ${buttonObj.pill ? 'rounded-full':'rounded-md'}
+            ${buttonObj.size == 'small' ? 'small' : buttonObj.size == 'medium' ? 'medium': buttonObj.size = 'big' ? 'big':''}
             ${buttonObj.icon ? 'icon':''}
-            `}>{buttonObj.label}
+            `}
+            target={`${buttonObj.target ? '_blank':'_self'}`}>{buttonObj.label}
             {buttonObj.icon && <span className="ico"><ReactSVG src="https://bplgtest.wpengine.com/wp-content/uploads/2023/02/arrow.svg" /></span>}
             </a>
         </Link>
