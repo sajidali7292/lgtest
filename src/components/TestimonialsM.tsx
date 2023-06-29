a10992739ff5037a7fb6249e2af4f9a48e1a0f17
femiss <felipe@linkgraph.io>
2023-06-29 11:00:20 -0600

    Updated | Testimonial Dynamic

--- src/components/TestimonialsM.tsx
+++ src/components/TestimonialsM.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styles from 'scss/components/TestimonialsM.module.scss';
 import Image from 'next/image';
+import { sectionVariantsTop, sectionVariantsBottom } from './constants';
 
 interface Props {
     title?: string;
@@ -13,15 +14,13 @@ interface Props {
 function TestimonialsM({
     title,
     testimonialsA,
-    pt = 't1',
-    pb = 'b1',
+    pt = 'md',
+    pb = 'md',
   }: Props): JSX.Element {
-    const paddingVariant = {
-      t1: 'pt-2', t2: 'pt-4', t3: 'pt-6', t4: 'pt-8', t5: 'pt-10', t6: 'pt-12', t7: 'pt-14', t8: 'pt-16',
-      b1: 'pb-2', b2: 'pb-4', b3: 'pb-6', b4: 'pb-8', b5: 'pb-10', b6: 'pb-12', b7: 'pb-14', b8: 'pb-16',
-    }
+    const ptVariant = sectionVariantsTop[pt];
+    const pBVariant = sectionVariantsBottom[pb];
     return (
-      <section className={`${styles.wrapper} ${paddingVariant[pt]} ${paddingVariant[pb]}`}>
+      <section className={`${styles.wrapper} ${ptVariant} ${pBVariant}`}>
         <div className={`${styles.wrap}`}>
           <div className={`${styles.wrap_testimonials} wrap_content wrap_content-small px-0 flex flex-row flex-wrap justify-center`} >
             {testimonialsA?.map((testimonial, index) => (
