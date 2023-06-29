5245a4261a752032dd20246d66bc4c54edc263d1
femiss <felipe@linkgraph.io>
2023-06-29 11:02:19 -0600

    Updated | Block and Link Dynamic

--- src/components/UI/LinkBlock.tsx
+++ src/components/UI/LinkBlock.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import styles from 'scss/components/ThumbnailButtons.module.scss';
+import classnames from 'classnames'
+
 
 interface LinkBlockProps {
   index: number;
@@ -9,38 +11,34 @@ interface LinkBlockProps {
     height: number;
     alt: string;
   };
+  Link_Tag?: string;
   Link_Title?: string;
   Link_URL: string;
   Link_ReadTime?: string;
   className?: string;
 }
 
-function LinkBlock ({ index, Link_Thumbnail, Link_Title, Link_URL, Link_ReadTime, className }: LinkBlockProps): JSX.Element {
-    return (
-        <div className={className}>
-            <div className={styles.linkBlocks}>
-                <a href={Link_URL} target="_blank" rel="noopener noreferrer">
-                    <div className={styles.imgWrapper}>
-                        <div className={`${styles.img} img-block-${index}`}>
-                            <img
-                                src={Link_Thumbnail.url}
-                                width={Link_Thumbnail.width}
-                                height={Link_Thumbnail.height}
-                                alt={Link_Thumbnail.alt? Link_Thumbnail.alt: `Image ${Link_Title}`}
-                            />
-                        </div>
-                    </div>
-                    <div className={`${styles.Link_Title}`}>{Link_Title}</div>
-                    <div className={`${styles.ReadTime}`}>
-                        {Link_ReadTime}
-                        <span>
-                            <img src="/wp-content/uploads/2022/08/shape.png" alt="shape" />
-                        </span>
-                    </div>
-                </a>
+function LinkBlock({ index, Link_Thumbnail, Link_Title, Link_URL, Link_ReadTime, className, Link_Tag }: LinkBlockProps): JSX.Element {
+  return (
+    <div className={`${className} flex flex-col`}>
+      <div className={`${styles.linkBlocks} linkBlocks flex flex-col h-full`}>
+        <a className={`flex flex-col justify-between h-full`} href={Link_URL} target="_blank" rel="noopener noreferrer">
+          <div className={`${styles.contentTop}`}>
+            <div className={`${classnames(styles.thumbnail_wrapper)} thumbnail_wrapper`} style={{backgroundImage: `url(${Link_Thumbnail.url})`}}>
+              {Link_Tag && <div className={`Link_Tag ${styles.Video_Tag}`}>{Link_Tag}</div>}
             </div>
-        </div>
-    );
-};
+            <div className={`${styles.Link_Title} Link_Title`}>{Link_Title}</div>
+          </div>
+          <div className={`${styles.ReadTime} ReadTime`}>
+            {Link_ReadTime}
+            <span>
+              <img src={`${process.env.NEXT_PUBLIC_WORDPRESS_URL}/wp-content/uploads/2023/03/shape.webp`} alt="shape" />
+            </span>
+          </div>
+        </a>
+      </div>
+    </div>
+  );
+}
 
 export default LinkBlock;
--- src/components/UI/VideoBlock.tsx
+++ src/components/UI/VideoBlock.tsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import styles from 'scss/components/ThumbnailButtons.module.scss';
 import Modal from './Modal';
+import classnames from 'classnames';
+
 
 interface VideoBlockProps {
   index: number;
@@ -10,13 +12,14 @@ interface VideoBlockProps {
     height: number;
     alt: string;
   };
+  Video_Tag?: string;
   Video_Title?: string;
   Video_ReadTime?: string;
   modalContent?: JSX.Element;
   className?: string;
 }
 
-function VideoBlock ({ index, Video_Thumbnail, Video_Title, Video_ReadTime, modalContent, className }: VideoBlockProps): JSX.Element {
+function VideoBlock ({ index, Video_Thumbnail, Video_Title, Video_ReadTime, modalContent, className, Video_Tag }: VideoBlockProps): JSX.Element {
     const [showModal, setShowModal] = useState(false);
 
     const handleModalButtonClick = () => {
@@ -28,24 +31,21 @@ function VideoBlock ({ index, Video_Thumbnail, Video_Title, Video_ReadTime, moda
     }
 
     return (
-        <div className={className}>
-            <div className={styles.videoBlocks}>
-                <div className={styles.modalOpen} onClick={handleModalButtonClick}>
-                    <div className={`${styles.img} img-block-${index}`}>
-                        <div className={`${styles.thumbnail_wrapper}`}>
-                            <img
-                                src={Video_Thumbnail.url}
-                                width={Video_Thumbnail.width}
-                                height={Video_Thumbnail.height}
-                                alt={Video_Thumbnail.alt? Video_Thumbnail.alt: `Image ${Video_Title}`}
-                            />
+        <div className={`${className} flex flex-col`}>
+            <div className={`${styles.videoBlocks} videoBlocks flex flex-col h-full`}>
+                <div className={`${styles.modalOpen} flex flex-col justify-between h-full`} onClick={handleModalButtonClick}>
+                    <div className={`${styles.contentTop}`}>
+                        <div className={`${styles.img} img img-block-${index}`}>
+                            <div className={classnames(styles.thumbnail_wrapper, 'thumbnail_wrapper')} style={{ backgroundImage: `url(${Video_Thumbnail.url})` }}>
+                                {Video_Tag && <div className={`Video_Tag ${styles.Video_Tag}`}>{Video_Tag}</div>}
+                            </div>
                         </div>
+                        <div className={`${styles.Video_Title} Video_Title`}>{Video_Title}</div>
                     </div>
-                    <div className={`${styles.Video_Title}`}>{Video_Title}</div>
-                    <div className={`${styles.Video_ReadTime}`}>
+                    <div className={`${styles.Video_ReadTime} Video_ReadTime`}>
                         {Video_ReadTime}
                         <span>
-                            <img src="/wp-content/uploads/2022/08/shape.png" alt="shape" />
+                            <img src={`${process.env.NEXT_PUBLIC_WORDPRESS_URL}/wp-content/uploads/2023/03/shape.webp`} alt="shape" />
                         </span>
                     </div>
                 </div>
