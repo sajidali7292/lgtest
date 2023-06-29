a3f88e3b8982bec1a7a7a41b94f2a90aebcc2712
femiss <felipe@linkgraph.io>
2023-06-29 10:54:44 -0600

    Updated | CaseStudy structure

--- src/components/CaseStudyHome.tsx
+++ src/components/CaseStudyHome.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import Image from 'next/image';
 import styles from 'scss/components/CaseStudyHome.module.scss';
 import Button from './UI/Button/index';
+import BadgeRow from './UI/BadgeRow';
+import { sectionVariantsTop, sectionVariantsBottom } from './constants';
 
 interface Props {
-  tag?: string;
+  tag?: any;
   title?: string;
   text?: string;
   quote?: string;
@@ -18,6 +21,13 @@ interface Props {
     alt?: string;
   };
   button?: any;
+  badgeObj?: any;
+  bgImage1?: string;
+  bgColor1?: string;
+  bgImage2?: string;
+  bgColor2?: string;
+  pt?: string;
+  pb?: string;
 }
 
 function CaseStudyHome({
@@ -30,39 +40,72 @@ function CaseStudyHome({
   position,
   logo,
   button,
+  badgeObj,
+  bgImage1,
+  bgColor1,
+  bgImage2,
+  bgColor2,
+  pt = 'md',
+  pb = 'md',
 }: Props): JSX.Element {
+  const ptVariant = sectionVariantsTop[pt];
+  const pBVariant = sectionVariantsBottom[pb];
+
+  const bgStyle1 = 
+    bgImage1 ? { 
+      backgroundImage: `url(${bgImage1?.url})`,
+      backgroundPosition: bgImage1?.position ? bgImage1.position:'center',
+      backgroundSize: bgImage1?.size ? bgImage1.size:'cover',
+      backgroundRepeat: bgImage1?.repeat ? bgImage1.repeat:'no-repeat'
+    } : {};
+  const bgStyle2 = 
+    bgImage2 ? { 
+      backgroundImage: `url(${bgImage2?.url})`,
+      backgroundPosition: bgImage2?.position ? bgImage2.position:'center',
+      backgroundSize: bgImage2?.size ? bgImage2.size:'cover',
+      backgroundRepeat: bgImage2?.repeat ? bgImage2.repeat:'no-repeat'
+    } : {};
+
   return (
-    <section className={styles.trust}>
-      <div className="container">
-        <div className="flex flex-row">
-          <div className={`basis-full lg:basis-6/12 ${styles.bg_trustG}`}>
+    <section className={`${styles.trustC} ${ptVariant} ${pBVariant}`}>
+      <div className={`container ${styles.containerCase}`}>
+        <div className={`flex flex-row ${styles.rowCase}`}>
+          <div className={`basis-full lg:basis-6/12 ${styles.bg_trustC} bg-${bgColor1}`} style={bgStyle1}>
             <div className={styles.trust_col1}>
-              <div className={styles.int_subHead}>{tag}</div>
+              { tag && <div className={`${styles.int_subHead} tag tag_${tag.color}`}>{tag.name} {tag.span && <span>{tag.span}</span> }</div>}
               <h2 className={styles.int_title}>{title}</h2>
               <div className={styles.int_txt}>{text}</div>
               {button && (
-                  <Button buttonObj={button} />
-                )}
-              <div className={styles.bottom_txt}>
-                Best B2B SEO Campaign 2021 - <span><img src="/wp-content/uploads/2022/03/Group2.svg" alt="" /></span> The Drum Awards
-              </div>
+                <Button buttonObj={button} />
+              )}
+              {badgeObj && (
+                <BadgeRow badgeObj={badgeObj}/>
+              )}
             </div>
           </div>
-          <div className={`basis-full lg:basis-6/12 ${styles.bt_trustY}`}>
+          <div className={`basis-full lg:basis-6/12 ${styles.bg_trustC} bg-${bgColor2}`} style={bgStyle2}>
             <div className={styles.trust_col2}>
               <div className={styles.trust_quote}>{quote}</div>
-              <div className="flex flex-row">
-                <div className="basis-6/12 photoCaseStudyHome">
-                  <img src={photo.url} alt={photo.alt} className={styles.trust_cPhoto} />
-                </div>
-                <div className="basis-6/12 txtCaseStudyHome">
-                  <div className={styles.trust_cName}>{name}</div>
-                  <div className={styles.trust_cPosition}>{position}</div>
-                  <div className={styles.trust_cLogo}>
-                    <img src={logo.url} alt={logo.alt} />
+              { name &&
+                <div className={`flex flex-row items-center ${styles.caseWrapper}`}>
+                  <div className={`flex-none ${styles.photoCaseStudyHome}`}>
+                    <Image className="object-contain object-right"
+                      src={photo.url} alt={photo.alt} className={styles.trust_cPhoto}
+                      width={`${photo.width ? photo.width:'77'}`}
+                      height={`${photo.height ? photo.height:'77'}`}/>
+                  </div>
+                  <div className={`basis-7/12 ${styles.txtCaseStudyHome}`}>
+                    <div className={styles.trust_cName}>{name}</div>
+                    <div className={styles.trust_cPosition}>{position}</div>
+                    <div className={styles.trust_cLogo}>
+                      <Image className="object-contain object-right"
+                        src={logo.url} alt={logo.alt}
+                        width={`${logo.width ? logo.width:'150'}`}
+                        height={`${logo.height ? logo.height:'20'}`}/>
+                    </div>
                   </div>
                 </div>
-              </div>
+              }
             </div>
           </div>
         </div>
