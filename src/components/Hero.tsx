a2f3736e12440ceedb82b01dd0ee2db5753992ad
femiss <felipe@linkgraph.io>
2023-08-24 12:45:32 -0600

    Updated | New variables

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,51 +1,55 @@
 import React from 'react';
 import styles from 'scss/components/Hero.module.scss';
+import Image from 'next/image';
+import Button from './UI/Button/index';
 
 interface Props {
   title?: string;
+  description?: string;
   id?: string;
   bgImage?: string;
-  buttonText?: string;
-  buttonURL?: string;
-  button2Text?: string;
-  button2URL?: string;
+  buttons?: any;
   children?: React.ReactNode;
 }
 
 function Hero({
   title = '',
+  description,
   id,
   bgImage,
-  buttonText,
-  buttonURL,
-  button2Text,
-  button2URL,
+  buttons,
   children,
 }: Props): JSX.Element {
   return (
     <section
       // eslint-disable-next-line react/jsx-props-no-spreading
       {...(id && { id })}
-      style={{ backgroundImage: bgImage ? `url(${bgImage})` : 'none' }}
+      // style={{ backgroundImage: bgImage ? `url(${bgImage})` : 'none' }}
       className={styles.hero}>
-      <div className={styles.wrap}>
-        <h1>{title}</h1>
-        <div className={styles.intro}>
-          <div className={styles.children}>{children}</div>
-          {buttonText && buttonURL && (
-            <p>
-              <a href={buttonURL} className="button">
-                {buttonText}
-              </a>
-            </p>
-          )}
-          {button2Text && button2URL && (
-            <p>
-              <a href={button2URL} className="button button-secondary">
-                {button2Text}
-              </a>
-            </p>
-          )}
+      <div className={`wrapper_content container`}>
+        <div className={`wrap_content wrap_content-extended flex flex-row flex-wrap relative ${styles.wrap}`}>
+          <div className={`hidden md:block `}>
+            <Image 
+              src={bgImage}
+              className={`object-contain object-right lg:object-right-bottom`}
+              layout='fill'
+            />
+          </div>
+          <div className={`w-full md:w-1/2 relative ${styles.colWrap}`}>
+            <div className={`${styles.titleTop}`}>{title}</div>
+            <div className={`${styles.intro}`}>
+              <div className={`${styles.descriptionTop}`}>{description}</div>
+              {buttons && (
+                <div className={`flex flex-row flex-wrap`}>
+                  {buttons &&
+                    buttons.map((button, index) => (
+                      <Button buttonObj={button} key={index} />
+                    ))
+                  }
+                </div>
+              )}
+            </div>
+          </div>
         </div>
       </div>
     </section>
