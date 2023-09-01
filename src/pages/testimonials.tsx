f29461022dd7996aea5ccc08d6bd8f2d86d0dcc7
LinkGraph2021 <projects@linkgraph.io>
2023-09-01 17:24:07 -0500

    Update testimonials.tsx

--- src/pages/testimonials.tsx
+++ src/pages/testimonials.tsx
@@ -25,6 +25,7 @@ export default function Team() {
               <TestimonialInfo key={index} testimonial={testimonial} />
             ))}
           </div>
+          <div>Test text</div>
         </div>
       </main>
 
