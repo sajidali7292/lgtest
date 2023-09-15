4467d20da5e347290cb069ff97bae074995de6c5
Judagutor <judagutor@gmail.com>
2023-09-15 12:59:04 -0500

    FAQ custom page

--- src/pages/custom-page.tsx
+++ src/pages/custom-page.tsx
@@ -4,6 +4,7 @@ import { Hero } from 'components';
 import { GetStaticPropsContext } from 'next';
 import Head from 'next/head';
 import PricingTab from 'components/PricingTab';
+import styles from 'scss/components/PricingTabTemp.module.scss';
 
 export default function Page() {
   const { useQuery } = client;
@@ -37,47 +38,3 @@ export async function getStaticProps(context: GetStaticPropsContext) {
     client,
   });
 }
-
-import React, { useRef } from 'react';
-import { WebView } from 'react-native-webview';
-
-const MyComponent = () => {
-  const webViewRef = useRef<WebView | null>(null);
-
-  const styleContent = `
-    body {
-      background-color: lightblue;
-    }
-    h1 {
-      color: red;
-    }
-  `;
-
-  const updateStyle = () => {
-    const newStyleContent = `
-      body {
-        background-color: green;
-      }
-    `;
-
-    // Inject JavaScript to update the style
-    const script = `
-      const styleElement = document.createElement('style');
-      styleElement.textContent = \`${newStyleContent}\`;
-      document.head.appendChild(styleElement);
-    `;
-
-    webViewRef.current?.injectJavaScript(script);
-  };
-
-  return (
-    <WebView
-      ref={webViewRef}
-      originWhitelist={['*']}
-      source={{ html: `<html><body><h1>Hello World</h1></body></html>` }}
-    />
-    <button onPress={updateStyle}>Change Style</button>
-  );
-};
-
-export default MyComponent;
